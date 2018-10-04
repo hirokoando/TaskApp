1aab4b4434837f7c876021295ac79266f32ce878
hirokoando <01074ahinrodoko4@gmail.com>
2018-10-04 15:37:19 +0900

    Initial commit

---
+++ app/src/main/java/jp/techacademy/hiroko/ando/taskapp/MainActivity.java
@@ -0,0 +1,28 @@
+package jp.techacademy.hiroko.ando.taskapp;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ListView;
+
+public class MainActivity extends AppCompatActivity {
+    private ListView mListView;
+    private TaskAdapter mTaskAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+    }
+}
